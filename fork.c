4ddc98c8581968bac7fc13dc0b781a14c5e5d645
JulianG91012 <juliangomez91012@gmail.com>
2022-08-24 20:38:04 -0500

    Basic functions finished

--- fork.c
+++ fork.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+int contador;
 int main()
 {
-    // fork();
-    printf("Hola Sistemas Operativos\n %d\n", getpid());
-    // while (1);
+    int pid, pid1, pid2;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Fallo del fork");
+    }
+    else if (pid == 0)
+    {
+        wait(NULL);
+        printf("Hijo[1] --> PID: %d, PPID: %d\n", getpid(), getppid());
+    }
+    else
+    {
+        pid1 = fork();
+        if (pid1 == 0)
+        {
+            wait(NULL);
+            printf("Hijo[2] --> PID: %d, PPID: %d\n", getpid(), getppid());
+        }
+        else
+        {
+            pid2 = fork();
+            if (pid2 == 0)
+            {
+                printf("Hijo[3] --> PID: %d, PPID: %d\n", getpid(), getppid());
+            }
+            else
+            {
+                printf("Padre --> PID: %d, PPID: %d\n", getpid(), getppid());
+            }
+        }
+    }
     return 0;
 }
