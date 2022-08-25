690fa4b30033fb688a13354b4b7fd04b7ae479b6
JulianG91012 <juliangomez91012@gmail.com>
2022-08-24 21:22:56 -0500

    Code Cleanup and Comments added

--- fork.c
+++ fork.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+// Variable global para crear copias en cada uno de los hijos
 int contador = 0;
-
 int main()
 {
     int pid, pid1, pid2;
-
     pid = fork();
-
+    
     if (pid < 0)
     {
         printf("Fallo del fork");
     }
     else if (pid == 0)
     {
+        //Se usan los wait para que no compitan por los recursos de la máquina
         wait(NULL);
+        // Acumulador de los hijos
         for (int i = 0; i <= 5; i++)
         {
             contador = i + contador;
+            // Se muestra el PID del hijo actual
             printf("Hijo[1] --> PID: %d, PPID: %d ID: %d\n", getpid(), getppid(), contador);
         }
         printf("\n");
@@ -29,6 +31,7 @@ int main()
         pid1 = fork();
         if (pid1 == 0)
         {
+            //Se usan los wait para que no compitan por los recursos de la máquina
             wait(NULL);
             for (int i = 0; i <= 5; i++)
             {
@@ -42,6 +45,8 @@ int main()
             pid2 = fork();
             if (pid2 == 0)
             {
+                //Se usan los wait para que no compitan por los recursos de la máquin
+                wait(NULL);
                 for (int i = 0; i <= 5; i++)
                 {
                     contador = i + contador;
@@ -51,6 +56,7 @@ int main()
             }
             else
             {
+                //Se agregan 3 waits, para que el padre espere a que los 3 hijos terminen para realizar sus operaciones
                 wait(NULL);
                 wait(NULL);
                 wait(NULL);
